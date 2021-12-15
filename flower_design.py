04c8fc2c79077a74f7ee36dd8a72a317957285c0
AYUSHI VATS <57026946+Ayushi321@users.noreply.github.com>
2021-12-15 23:36:03 +0530

    Flower Design

---
+++ flower_design.py
@@ -0,0 +1,11 @@
+import turtle
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor('white')
+t.pencolor('green')
+t.speed(0)
+for i in range(150):
+    t.circle(190-1,90)
+    t.lt(98)
+    t.circle(190-1,90)
+    t.lt(18)
